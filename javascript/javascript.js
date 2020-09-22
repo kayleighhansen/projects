a4e566d115605e7b86f1ac603595906a31db6612
pardonmygiraffee <han16017@byui.edu>
2020-09-22 13:07:04 -0600

    ....

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,6 @@
 let d = new Date ();
-let dayName = d.getDay();
-let monthName = d.getMonth();
 let year = d.getFullYear();
+document.getElementById("copywriteYear").textContent = year;
 
-let fullDate = dayName + "/" + monthName+ "/" + year;
-document.getElementById("fullDate").textContent = currentDate;
-
-let a = 1;
-document.getElementById("a").textContent = currentDate;
+let lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
