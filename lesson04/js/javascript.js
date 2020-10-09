f6526328aacb2614635d23d90e9c91cca204e972
pardonmygiraffee <han16017@byui.edu>
2020-10-09 13:42:21 -0600

    ...

--- lesson04/js/javascript.js
+++ lesson04/js/javascript.js
@@ -4,3 +4,10 @@ const mainnav = document.querySelector('.navigation')
 menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
 window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
+
+let d = new Date ();
+let year = d.getFullYear();
+document.getElementById("copywriteYear").textContent = year;
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
