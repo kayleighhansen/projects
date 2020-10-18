df1739df0f734b438f1b0a191a787eaf0d5df3ef
pardonmygiraffee <han16017@byui.edu>
2020-10-17 23:41:27 -0400

    ...

---
+++ lesson05/js/javascript.js
@@ -0,0 +1,20 @@
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
+
+
+let d = new Date ();
+let year = d.getFullYear();
+document.getElementById("copywriteYear").textContent = year;
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById("currentDate").textContent = today;
