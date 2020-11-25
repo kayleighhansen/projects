80cc583a4e4a77d931318b06c42e63bd4fdfbc01
pardonmygiraffee <han16017@byui.edu>
2020-11-25 07:51:00 -0500

    ...

---
+++ lesson11/fish-haven-page/javascript/javascript.js
@@ -0,0 +1,29 @@
+//Navigation Stuff
+
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+window.onresize = () => { if (window.innerWidth > 800) mainnav.classList.remove('responsive') };
+
+//Alert Stuff
+
+var day = new Date();
+if (day.getDay() == 5) {
+  document.getElementById("alert").style.backgroundColor= "yellow";
+  document.getElementById("alert").style.display= "block";
+  document.getElementById("alert").style.fontWeight= "bold";
+}
+
+
+//Footer Stuff
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+var yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById("currentDate").textContent = today;
+
+
+
+
