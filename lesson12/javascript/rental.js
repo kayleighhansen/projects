25567b560205b14b1ee45f44e1514a3772a91922
pardonmygiraffee <han16017@byui.edu>
2020-12-07 14:55:17 -0500

    ...

--- lesson12/javascript/rental.js
+++ lesson12/javascript/rental.js
@@ -1,2 +1,10 @@
-let rental_json = 'lesson12/javascript/rentals.json';
-console.log(rental_json);
+const rentalurl = 'https://pardonmygiraffee.github.io/wdd230/lesson12/javascript/rentals.json';
+
+fetch(rentalurl)
+    .then(function (response) {
+        return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  
+    const data = jsonObject['data'];
+  });
