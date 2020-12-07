c4b9a1abdff942311c4edcadd8d1891b5c0bcc3a
pardonmygiraffee <han16017@byui.edu>
2020-12-07 15:14:00 -0500

    ...

--- lesson12/javascript/rental.js
+++ lesson12/javascript/rental.js
@@ -3,8 +3,14 @@ const rentalurl = 'https://pardonmygiraffee.github.io/wdd230/lesson12/javascript
 fetch(rentalurl)
     .then(function (response) {
         return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  
-    const data = jsonObject['data'];
+    })
+    .then(function (jsonObject) {
+        console.log(jsonObject);  
+        const data = jsonObject['data'];
+        for (let i = 0; i < 6; i++ ) {
+
+            console.log(pioneer[0]["max-persons"])
+            let name = pioneer[0]["max-persons"];
+            document.getElementById('scooter-name-' + i).innerText = name;
+        }
   });
