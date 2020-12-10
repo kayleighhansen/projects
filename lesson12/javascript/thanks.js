3a7712ed6010c7a04bb82a2a5b5538c4a525adbc
pardonmygiraffee <han16017@byui.edu>
2020-12-09 19:17:47 -0500

    ...

---
+++ lesson12/javascript/thanks.js
@@ -0,0 +1,11 @@
+type = "honda";
+document.getElementById("thanks-type").innerText = type;
+
+number = 1;
+document.getElementById("thanks-number").innerText = number;
+
+date = "10.20.1300";
+document.getElementById("thanks-date").innerText = date;
+
+period = 2 + " days";
+document.getElementById("thanks-period").innerText = period;
