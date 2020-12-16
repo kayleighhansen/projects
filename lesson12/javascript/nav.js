70d409c9ef5131ca3c2c5be9a4ed46fa5786f53d
pardonmygiraffee <han16017@byui.edu>
2020-12-16 16:46:39 -0500

    ...

--- lesson12/javascript/nav.js
+++ lesson12/javascript/nav.js
@@ -2,3 +2,9 @@ const menubutton = document.querySelector('.astericks-menu');
 const mainnav = document.querySelector('.navigation')
 menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 window.onresize = () => { if (window.innerWidth > 800) mainnav.classList.remove('responsive') };
+
+
+//Footer Last Modified
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
